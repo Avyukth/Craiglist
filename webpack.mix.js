29d1227bfee5da822673cc3072b3835cde71ba4d
subha <subhrajit.makur@razorpay.com>
2019-07-03 12:51:04 +0530

    admin page added

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles([
+    'resources/assets/css/libs/blog-post.css',
+    'resources/assets/css/libs/bootstrap.css',
+    'resources/assets/css/libs/font-awesome.css',
+    'resources/assets/css/libs/metisMenu..css',
+    'resources/assets/css/libs/sb-admin-2.css',
+    'resources/assets/css/libs/styles.css',
+
+],'public/css/libs.css');
+
+
+mix.scripts([
+    'resources/assets/js/libs/blog-post.js',
+    'resources/assets/js/libs/bootstrap.js',
+    'resources/assets/js/libs/font-awesome.js',
+    'resources/assets/js/libs/metisMenu.js',
+    'resources/assets/js/libs/sb-admin-2.js',
+    'resources/assets/js/libs/styles.js',
+],'public/js/libs.js');
