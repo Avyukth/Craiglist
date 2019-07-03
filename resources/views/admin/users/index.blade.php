baa0ae813ab4d4356949841ce574db53e495ca2f
subha <subhrajit.makur@razorpay.com>
2019-07-03 11:18:15 +0530

    view added

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>users</h1>
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        @if($users)
+                @foreach($users as $user)
+                  <tr>
+                    <td>{{$user->id}}</td>
+                      <td>{{$user->name}}</td>
+                      <td>{{$user->email}}</td>
+                      <td>{{$user->created_at}}</td>
+                      <td>{{$user->updated_at}}</td>
+                  </tr>
+                @endforeach
+          @endif
+        </tbody>
+    </table>
+
+@stop
